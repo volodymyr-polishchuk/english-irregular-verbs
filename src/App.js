a64d5c332195f8d76791e1420394d032db35e1f8
Volodymyr Polishchuk <volodymyrpo@tahometer.com>
2019-06-09 22:41:44 +0300

    Create project skeleton

--- src/App.js
+++ src/App.js
@@ -1,26 +1,65 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Button, Container, Form, FormControl, FormGroup,} from "react-bootstrap";
+import 'bootstrap/dist/css/bootstrap.css';
+import out from './output.json';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+    state = {
+        arr: [...out],
+        element: [...out][Math.floor(Math.random() * [...out].length)],
+        counter: 0,
+        rightCounter: 0,
+        lastRightAnswer: ''
+    };
+
+    inputRef = React.createRef();
+
+    handlerCheckClick = () => {
+        const input = this.inputRef.current;
+        const value = input.value;
+        if (value === this.state.element.pastSimple) {
+            this.setState((state, props) => ({
+                rightCounter: state.rightCounter + 1
+            }));
+        }
+        let newElement = this.getNewElement();
+        this.setState((state, props) => ({
+            counter: state.counter + 1,
+            lastRightAnswer: state.element.pastSimple,
+            element: newElement,
+        }));
+        input.value = '';
+    };
+
+    getNewElement = () => {
+        const number = Math.floor(Math.random() * this.state.arr.length);
+        return this.state.arr[number];
+    };
+
+    submit = (e) => {
+        e.preventDefault();
+        this.handlerCheckClick();
+    };
+
+    render() {
+        return (
+            <Container>
+                <h2>Word: {this.state.element.infinitive}</h2>
+                <h4>Answers (right/all): {this.state.rightCounter}/{this.state.counter}</h4>
+                <Form onSubmit={this.submit}>
+                    <FormGroup>
+                        <FormControl type="text" placeholder="Past Simple" ref={this.inputRef}>
+
+                        </FormControl>
+                    </FormGroup>
+                </Form>
+                <Button onClick={this.handlerCheckClick}>Check</Button>
+                <h4>{this.state.lastRightAnswer}</h4>
+            </Container>
+        );
+    }
 }
 
 export default App;
